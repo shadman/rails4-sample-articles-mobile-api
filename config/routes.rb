509f97303c7f58d6480bf9e216455f1a3e66c4a5
Shadman <shadman.jamil@tenpearls.com>
2016-09-06 23:19:31 +0500

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-	get '/test' => 'resume#index'
+	get '/:owner_name/articles' => 'articles#list'
+	get '/articles/:id' => 'articles#view'
+
+	get '/owners' => 'owners#list'
+	get '/:owner_name' => 'owners#view'
 
 end

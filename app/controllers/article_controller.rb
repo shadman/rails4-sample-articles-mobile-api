445881aab0ab04df0532216f267ad881ad67abcf
Shadman <shadman.jamil@tenpearls.com>
2016-09-06 23:50:02 +0500

    comments added for methods

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -3,6 +3,8 @@ class ArticleController < ApplicationController
   # Method GET
   # Param: owner_name
   # Example: '/:owner_name/articles'
+  # JSON Success Output: { "result": [ {"id": 1, "name": "Test name", "price": 33.22, "description": "Description test", "owner_name": "shadman"} ] } 
+  # JSON Error Output: { "result": { "error": { "message": "Sorry, no record(s) found." }
   def list
     owner_name = params[:owner_name]
     articles = Article.find_by_owner_name owner_name
@@ -11,12 +13,15 @@ class ArticleController < ApplicationController
     response_200 articles
   end
 
+
   # Method GET
   # Param: id
   # Example: /articles/:id'
+  # JSON Success Output: { "result": {"id": 1, "name": "Test name", "price": 33.22, "description": "Description test", "owner_name": "shadman"} } 
+  # JSON Error Output: { "result": { "error": { "message": "Sorry, invalid request found." }
   def view
     article_id = params[:id]
-    article = Article.find_by_id article_id
+    article = Article.find_by_id_with_owner article_id
     response_400('invalid') if article.nil?
 
     response_200 article
--- app/controllers/owner_controller.rb
+++ app/controllers/owner_controller.rb
@@ -2,6 +2,8 @@ class OwnerController < ApplicationController
 
   # Method GET
   # Example: '/owners'
+  # JSON Success Output: { "result": [ {"id": 1, "name": "shadman"}, {"id": 2, "name": "matz"} ] } 
+  # JSON Error Output: { "result": { "error": { "message": "Sorry, no record(s) found." }
   def list
     owners = Owner.all_actived
     response_400('no_record') if owners.nil?
@@ -9,9 +11,12 @@ class OwnerController < ApplicationController
     response_200 owners
   end
 
+
   # Method GET
   # Param: owner_name
   # Example: '/:owner_name'
+  # JSON Success Output: { "result": {"id": 1, "name": "shadman"} } 
+  # JSON Error Output: { "result": { error: { "message": "Sorry, invalid request found." }
   def view
     owner_name = params[:owner_name]
     owner = Owner.find_owner_by_name owner_name
